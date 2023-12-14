e928e67b474612b5a1cc0eef08d2647f3f76ae6f
hobbyscripterII <wruomma2@naver.com>
2023-12-14 10:56:36 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/couple/couplediaryapp/CoupleDiaryAppApplication.java
@@ -0,0 +1,13 @@
+package com.couple.couplediaryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupleDiaryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupleDiaryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/couple/couplediaryapp/CoupleDiaryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couple.couplediaryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupleDiaryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
