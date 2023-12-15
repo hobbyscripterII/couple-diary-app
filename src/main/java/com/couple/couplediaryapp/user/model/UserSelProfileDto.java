d45935a02e95707474b6ab76e30e359d4eae493c
Noeul <lovesatr42@gmail.com>
2023-12-15 15:41:41 +0900

    feat: 배환 ( 프로필 작업, 일기 목록 작업 )

--- src/main/java/com/couple/couplediaryapp/user/model/UserSelProfileDto.java
+++ src/main/java/com/couple/couplediaryapp/user/model/UserSelProfileDto.java
@@ -1,6 +1,5 @@
 package com.couple.couplediaryapp.user.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
