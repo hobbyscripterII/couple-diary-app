9d8a680342413dd687b824666a5a59db667807f9
hobbyscripterII <wruomma2@naver.com>
2023-12-18 18:37:52 +0900

    dosc: 주석 수정

--- src/main/java/com/couple/couplediaryapp/user/model/UserUpdProfileDto.java
+++ src/main/java/com/couple/couplediaryapp/user/model/UserUpdProfileDto.java
@@ -1,17 +1,19 @@
 package com.couple.couplediaryapp.user.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
-
 public class UserUpdProfileDto {
-
     @JsonIgnore
     private int userId;
+    @Schema(title = "회원 이름")
     private String nm;
+    @Schema(title = "회원 프로필 사진")
     private String pic;
+    @Schema(title = "회원 생년월일")
     private String birth;
+    @Schema(title = "만난 날")
     private String startedAt;
-
 }
